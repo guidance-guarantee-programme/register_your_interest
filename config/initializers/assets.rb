0f93951b03ce6efcdcd47512bec0a940d62b50ac
Alistair Duggin <alistair.duggin@btinternet.com>
2014-10-17 11:05:17 +0100

    Set radio/checkbox focus state to include label
    Instead of the focus state being the element, we progressively enhance
    the behaviour so that the surrounding label shows the focus state.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'govuk_elements',
                                                          'public', 'sass')
 
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( register-your-interest.js )
