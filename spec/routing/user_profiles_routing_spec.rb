de41ea1dfa6991cbb3303a33e8e8b315a8b869cf
Gareth Visagie <gareth@gjvis.com>
2014-10-17 14:11:41 +0100

    Move all functionality to root path

--- spec/routing/user_profiles_routing_spec.rb
+++ spec/routing/user_profiles_routing_spec.rb
@@ -1,11 +1,11 @@
 RSpec.describe 'Routes for Registrations', type: :routing do
-  it 'routes GET /profile/register to user_profiles#new' do
-    expect(get: '/profile/register').
+  it 'routes GET / to user_profiles#new' do
+    expect(get: '/').
       to route_to(controller: 'user_profiles', action: 'new')
   end
 
-  it 'routes POST /profile to user_profiles#create' do
-    expect(post: '/profile').
+  it 'routes POST / to user_profiles#create' do
+    expect(post: '/').
       to route_to(controller: 'user_profiles', action: 'create')
   end
 end
