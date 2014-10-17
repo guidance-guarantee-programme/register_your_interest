a6cba5d3427483bc4451a65277ebb6337e0eac97
Alistair Duggin <alistair.duggin@btinternet.com>
2014-10-17 11:07:49 +0100

    Setup browser specific stylesheets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,6 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'govuk_elements',
                                                          'public', 'sass')
 
-Rails.application.config.assets.precompile += %w( register-your-interest.js )
+Rails.application.config.assets.precompile += %w( register-your-interest.js register-your-interest.css
+                                                  register-your-interest-ie6.css register-your-interest-ie7.css
+                                                  register-your-interest-ie8.css )
