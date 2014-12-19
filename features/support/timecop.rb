caf97b5abe5ab262a621406593aa414c34bf118d
Gareth Visagie <gareth@gjvis.com>
2014-12-19 16:03:50 +0000

    Freeze time with zero-ed out seconds for cucumber

--- features/support/timecop.rb
+++ features/support/timecop.rb
@@ -1,5 +1,6 @@
 require 'timecop'
 
 Around do |scenario, block|
-  Timecop.freeze { block.call }
+  # Zero out seconds to prevent time resolution differences breaking tests
+  Timecop.freeze(Time.utc(2014,"Sep",4,10,15,00)) { block.call }
 end
