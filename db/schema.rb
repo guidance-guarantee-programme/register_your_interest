0265b3ccba5cb7a33b505be6f6ac28babedde917
Andrew Garner <andrew@andrewgarner.com>
2015-01-09 15:41:14 +0000

    Add `updated_at` timestamp to user profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150109153732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 0) do
     t.text     "wishlist"
     t.text     "region"
     t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
