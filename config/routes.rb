aceea03ba47be7ba2f5424305139efb5767b9965
Andrew Garner <andrew@andrewgarner.com>
2015-01-21 10:34:02 +0000

    Only accept requests for HTML resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: redirect(ENV.fetch('ROOT_REDIRECT', '/register'))
 
-  get '/register', as: 'new_user_profile', to: 'user_profiles#new'
-  post '/register', as: 'user_profile', to: 'user_profiles#create'
+  constraints format: 'html' do
+    get '/register', as: 'new_user_profile', to: 'user_profiles#new'
+    post '/register', as: 'user_profile', to: 'user_profiles#create'
+  end
 end
