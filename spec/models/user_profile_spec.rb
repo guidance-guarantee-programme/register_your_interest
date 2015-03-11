835730fea3727ef6b0ef2ea8777053d1bb4ec636
Gareth Visagie <gareth@gjvis.com>
2015-03-11 14:29:22 +0000

    Add phone to user_profiles

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -2,6 +2,9 @@ RSpec.describe UserProfile, type: :model do
   it { is_expected.to respond_to :email }
   it { is_expected.to respond_to :email= }
 
+  it { is_expected.to respond_to :phone }
+  it { is_expected.to respond_to :phone= }
+
   it { is_expected.to respond_to :name }
   it { is_expected.to respond_to :name= }
 
